8e1400fda228d988dccc5635493d657821bead30
github username <github email address>
2019-10-08 16:41:23 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,49 @@
+class Dog
+    def hungry
+        puts "Eats dog food!"
+    end
+end 
+
+
+class Dog
+    def bark
+        puts "Bark, bark, bark, bark, bark!"
+    end
+end
+
+fido = Dog.new
+fido.bark 
+
+class Dog
+    def bark
+        puts "Woof!"
+    end
+end
+
+fido = Dog.new
+fido.bark 
+
+class Dog
+    def sit
+        puts "Please sit!"
+    end
+end
+
+fido = Dog.new
+fido.sit 
+
+
+class Dog
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+fido = Dog.new
+fido.sit 
+
+
+
+
+
+
